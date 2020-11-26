6783472228c12934c22d7ad19ec6c3e06500f367
David <david.krtolica8@gmail.com>
2020-11-26 13:39:08 +0100

    Started implementation of user profile page with thymeleaf.

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -4,6 +4,7 @@ import com.example.demo.model.User;
 import com.example.demo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.context.request.WebRequest;
@@ -16,6 +17,9 @@ public class UserController {
     @Autowired
     UserRepository userRepository;
 
+    //GLOBAL VARIABLE FOR THE LOGGED-IN USER
+    User loggedInUser;
+
     @GetMapping("/index")
     public String returnIndex() {
         return "/index";
@@ -23,7 +27,8 @@ public class UserController {
 
     //TEST METHOD FOR THE PROFILEPAGE.HTML, REMOVE LATER
     @GetMapping("/userpage")
-    public String returnProfilePage() {
+    public String returnProfilePage(Model model) {
+        model.addAttribute("loggedInUser", loggedInUser);
         return "/userpage";
     }
 
@@ -35,6 +40,8 @@ public class UserController {
 
         //JPA REPOSITORY METHOD WHICH RETURNS A LIST OF USERS WITH EMAIL AND PASSWORD PASSED IN THE INDEX.HTML
         List<User> usersByCredentials = userRepository.findUserByEmailAndPasswordContaining(email, password);
+        //SET THE LOGGED-IN USER TO A GLOBAL VARIABLE
+        loggedInUser = userRepository.findUserByEmailAndPasswordContaining(email, password).get(0);
 
         if (!usersByCredentials.isEmpty()){
             return "redirect:/userpage";
