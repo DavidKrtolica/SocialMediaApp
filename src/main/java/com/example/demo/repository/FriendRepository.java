9641266d60bafc024a2cfe45df7a8219f7fe5e44
David <david.krtolica8@gmail.com>
2020-11-27 15:06:30 +0100

    Implemented thymeleaf for showing the friend list on the user profile page.

--- src/main/java/com/example/demo/repository/FriendRepository.java
+++ src/main/java/com/example/demo/repository/FriendRepository.java
@@ -3,5 +3,9 @@ package com.example.demo.repository;
 import com.example.demo.model.Friend;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface FriendRepository extends JpaRepository<Friend, Integer> {
+    List<Friend> findByUserId(Integer userId);
+
 }
