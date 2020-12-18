87b3ff0602469ca7388371aa5169e58f6812bda2
David <david.krtolica8@gmail.com>
2020-12-18 11:22:53 +0100

    jUnit tests finished

--- src/test/java/com/example/demo/jUnitTests.java
+++ src/test/java/com/example/demo/jUnitTests.java
@@ -1,6 +1,7 @@
 package com.example.demo;
 
 import com.example.demo.model.Friend;
+import com.example.demo.model.Post;
 import com.example.demo.model.User;
 import com.example.demo.repository.FriendRepository;
 import com.example.demo.repository.PostRepository;
@@ -10,6 +11,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.List;
 
@@ -46,6 +48,50 @@ public class jUnitTests {
         userRepository.save(user);
         List<Friend> friends = friendRepository.findAll();
         assert(friends.isEmpty());
+    }
+
+    @Test
+    public void createsPost() {
+        postRepository.deleteAll();
+        Post post = new Post();
+        post.setPostDescription("blabla");
+        postRepository.save(post);
+        assertThat(postRepository.findById(post.getPostId()));
+    }
+
+    @Test
+    public void addsFriend() {
+        userRepository.deleteAll();
+        friendRepository.deleteAll();
+        User user1 = new User("David", "David", "blabla", "dd@gmail.com", "dd123");
+        User user2 = new User("Nirendra", "Rawal", "hehe", "nr@gmail.com", "nr123");
+        userRepository.save(user1);
+        userRepository.save(user2);
+        Friend friend1 = new Friend(user1.getUserId(), user2.getUserId(), user1, user2);
+        Friend friend2 = new Friend(user2.getUserId(), user1.getUserId(), user2, user1);
+        friendRepository.save(friend1);
+        friendRepository.save(friend2);
+        assertThat(friend1.getUserByUserId().equals(user1));
+    }
+
+    @Test
+    public void removesFriend() {
+        userRepository.deleteAll();
+        friendRepository.deleteAll();
+        User user1 = new User("David", "David", "blabla", "dd@gmail.com", "dd123");
+        User user2 = new User("Nirendra", "Rawal", "hehe", "nr@gmail.com", "nr123");
+        userRepository.save(user1);
+        userRepository.save(user2);
+        Friend friend1 = new Friend(user1.getUserId(), user2.getUserId(), user1, user2);
+        Friend friend2 = new Friend(user2.getUserId(), user1.getUserId(), user2, user1);
+        friendRepository.save(friend1);
+        friendRepository.save(friend2);
+        Friend friendcheck1 = friendRepository.findByUserIdAndFriendlyId(user1.getUserId(), user2.getUserId());
+        Friend friendcheck2 = friendRepository.findByUserIdAndFriendlyId(user2.getUserId(), user1.getUserId());
+        friendRepository.delete(friendcheck1);
+        friendRepository.delete(friendcheck2);
+        List<Friend> friendList = friendRepository.findAll();
+        assertThat(friendList.isEmpty());
     }
 
 }
